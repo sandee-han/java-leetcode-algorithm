d5afae243f16ae612430f9edbf500c6e3aea830a
sandee-han <iwmism1938@naver.com>
2023-04-18 18:24:46 +0900

    Time: 41 ms (66.56%), Space: 50.2 MB (44.83%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> answer = new LinkedList<>();
-        Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) {
-                continue;
-            }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
+    List<List<Integer>> answer = new ArrayList<>();
+    Arrays.sort(nums);
+    int base = 0;
+
+    while (base < nums.length - 2) {
+			
+        int left = base + 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int check =  nums[base] + nums[left] + nums[right];
+            if (check == 0) {
+                List<Integer> innerAnswer = new ArrayList<>();
+                innerAnswer.add(nums[base]);
+                innerAnswer.add(nums[left]);
+                innerAnswer.add(nums[right]);
+                answer.add(innerAnswer);
+                while (nums[left] == nums[left + 1] && left + 1 < right) {
                     left++;
-                } else {
-                    List<Integer> list = new LinkedList<>();
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    answer.add(list);
-                    left++;
-                    while (nums[left - 1] == nums[left] && right > left) {
-                        left++;
-                    }
                 }
+                left++;
+            } else if (check < 0) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return answer;
+
+        while (nums[base] == nums[base + 1] && base + 1 < nums.length - 2) {
+            base ++;
+        }
+        base++;
+    }
+    return answer;
     }
 }
